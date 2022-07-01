d39e4800284ee523a41e73301725c678c540cd44
Mirkos Martins <MirkosMartins@users.noreply.github.com>
2022-07-01 10:25:15 -0300

    update2

--- principal.py
+++ principal.py
@@ -3,6 +3,7 @@ import streamlit as st
 contador = 0
 
 st.text('APLICAÇÃO CONTADOR')
+st.input_text('Insira seu nome: ')
 if st.button('Incrementar'):
     contador+=1
     st.text('contador'+str(contador))
