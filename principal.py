f21ce2e14011ab752a3a8b4217be3f047594edae
Mirkos Martins <MirkosMartins@users.noreply.github.com>
2022-07-01 10:26:45 -0300

    add nome

--- principal.py
+++ principal.py
@@ -3,10 +3,11 @@ import streamlit as st
 contador = 0
 
 st.text('APLICAÇÃO CONTADOR')
-st.text_input('Insira seu nome: ')
+st.text_input('Insira seu nome: ',key='nome')
+nome = st.session_state.nome
 if st.button('Incrementar'):
     contador+=1
-    st.text('contador'+str(contador))
+    st.text(nome+' contador'+str(contador))
 if st.button('Decrementar'):
     contador-=1
-    st.text('contador'+str(contador))
+    st.text(nome+' contador'+str(contador))
