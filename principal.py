02bcd82b357f1352a78c489bb0c7c70b1a21b982
Mirkos Martins <MirkosMartins@users.noreply.github.com>
2022-07-01 10:23:11 -0300

    update1

--- principal.py
+++ principal.py
@@ -6,3 +6,6 @@ st.text('APLICAÇÃO CONTADOR')
 if st.button('Incrementar'):
     contador+=1
     st.text('contador'+str(contador))
+if st.button('Decrementar'):
+    contador-=1
+    st.text('contador'+str(contador))
