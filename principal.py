fe68a17bcf95b2def5b0109c551707028edd9291
Mirkos Martins <MirkosMartins@users.noreply.github.com>
2022-07-01 10:40:27 -0300

    add pandas

--- principal.py
+++ principal.py
@@ -1,13 +1,17 @@
 import streamlit as st
+import pandas as pd
 
-contador = 0
+valor = pd.read_csv('./data.csv',sep=';')#lê o valor de um csv
+
+contador = valor[0]
 
 st.text('APLICAÇÃO CONTADOR')
 st.text_input('Insira seu nome: ',key='nome')
 nome = st.session_state.nome
 if st.button('Incrementar'):
     contador+=1
-    st.text(nome+' contador'+str(contador))
 if st.button('Decrementar'):
     contador-=1
-    st.text(nome+' contador'+str(contador))
+st.text(nome+' contador'+str(contador))
+df= pd.DataFrame([contador],columns=['valor'])
+df.to_csv('./data.csv',sep=';')#salva o valor num csv
